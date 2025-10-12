868286571d49399b56f8d58036e6820541794938
soundararajan-uma <55054153+soundararajan-uma@users.noreply.github.com>
2025-10-11 21:25:31 -0400

    Add files via upload
    Day1 Assignment

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		String employeeName="Chris Tim";
+		int employeeAge=27;
+		char empGender='M';
+		float approxSalary=63045.87f;
+		boolean acceptTerms=true;
+		float avgRating=4.2f;
+		long mobileNumber=9361240506l;
+		int pinCode=630021;
+		char luckyNumber='5';
+		System.out.println("Employee Details of the Company:");
+		System.out.println("Name"+"      "+"Age"+"  "+"Gender"+" "+"Salary"+"  "+"Terms"+" "+"Rating"+" "+"MobileNo"+"   "+"Pincode"+"  "+"LuckyNo");
+		System.out.println(employeeName+" "+employeeAge+"   "+empGender+"     "+approxSalary+"  "+acceptTerms+"   "+avgRating+"  "+mobileNumber+" "+pinCode+"  "+luckyNumber);
+		
+
+	}
+
+}
